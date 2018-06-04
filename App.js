135f327cfc0abf71ebad483b590e9920b8eb7f73
Bee <thitisarnbee@gmail.com>
2018-06-04 21:56:34 +0700

    Init again :(

---
+++ App.js
@@ -0,0 +1,76 @@
+import React, { Component } from "react";
+import { StyleSheet, View } from "react-native";
+import { connect } from "react-redux";
+
+
+import PlaceInput from "./src/components/PlaceInput/PlaceInput";
+import PlaceList from "./src/components/PlaceList/PlaceList";
+import PlaceDetail from "./src/components/PlaceDetail/PlaceDetail";
+import {
+  addPlace,
+  deletePlace,
+  selectPlace,
+  deselectPlace
+} from "./src/store/actions/index";
+
+class App extends Component {
+  placeAddedHandler = name => {
+     this.props.onAddPlace(name);
+  };
+
+  placeDeleteHandler = () => {
+    this.props.onDeletePlace();
+  };
+
+  placeSelectedHandler = (key = 0) => {
+   this.props.onSelectPlace(key);
+  }
+
+  modalClosedHandler = () =>{
+    this.props.onDeselectPlace();
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <PlaceDetail
+          selectedPlace = {this.props.selectedPlace}
+          onItemDeleted={this.placeDeleteHandler}
+          onModalClosed={this.modalClosedHandler}
+        />
+        <PlaceInput  
+          onPlaceAdded={this.placeAddedHandler}  
+        />
+        <PlaceList places={this.props.places} onItemSelected={this.placeSelectedHandler} />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 26,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "flex-start"
+  }
+});
+
+const mapStateToProps = state =>{
+  return {
+    places:state.places.places,
+    selectedPlace:state.places.selectedPlace
+  }
+}
+
+const mapDispatchToProps = dispatch =>{
+  return {
+    onAddPlace: name => dispatch(addPlace(name)),
+    onDeletePlace: () => dispatch(deletePlace()),
+    onSelectPlace: key => dispatch(selectPlace(key)),
+    onDeselectPlace: () => dispatch(deselectPlace())
+  }
+}
+
+export default connect(mapStateToProps,mapDispatchToProps)(App);
